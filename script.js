8f1c0159b8afdf24b1c48a54c93e5260cc37eb92
Ghostified <bransonallan@gmail.com>
2024-07-13 23:53:52 +0300

    add an script to cross of items on the to do list

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+
+//function to store the text on the input box into the list container
 function addTask(){
     if (inputBox.value === '') {
         alert("You must write something!");
@@ -8,6 +10,26 @@ function addTask(){
     else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li)
+        listContainer.appendChild(li);
+        
+        //add a cross icon
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+
+        //Display the cross icon
+        li.appendChild(span);
     }
+
+    //clear the inputbox after a task has been added
+    inputBox.value = ""; 
 }
+
+//Click function on the x button
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
